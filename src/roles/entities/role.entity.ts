fb894b39ff170d14ae10b6d6e82cb35c3bc776d4
Valter Valik <valtervaralik@gmail.com>
2024-01-16 23:44:44 -0500

    refactor: :fire: Delete unused role-related files

--- src/roles/entities/role.entity.ts
+++ src/roles/entities/role.entity.ts
@@ -1,17 +1,9 @@
+import { Base } from 'src/base/base.entity';
 import { User } from 'src/users/entities/user.entity';
-import {
-  Column,
-  Entity,
-  JoinTable,
-  OneToMany,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Column, Entity, JoinTable, OneToMany } from 'typeorm';
 
 @Entity()
-export class Role {
-  @PrimaryGeneratedColumn()
-  id: number;
-
+export class Role extends Base {
   @Column()
   name: string;
 
