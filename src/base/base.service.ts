6f2292d38aa64f3109fb27f583db696675f58b70
Valter Valik <valtervaralik@gmail.com>
2024-02-09 21:54:54 -0500

    fix: :bug: add try catch in all methods and set new options in update methods

--- src/base/base.service.ts
+++ src/base/base.service.ts
@@ -15,6 +15,7 @@ import {
   Pagination,
   PaginationResult,
   Params,
+  UpdatedNew,
 } from './base-interfaces';
 
 type BaseServiceOptions = {
@@ -66,248 +67,285 @@ export function BaseService<T>(
       createDto: Params,
       activeUser?: ActiveUserData,
     ): Promise<T> {
-      const queryBuilder = this.genericRepository.createQueryBuilder();
-      const exists = await queryBuilder.select().where(conditions).getOne();
+      try {
+        const queryBuilder = this.genericRepository.createQueryBuilder();
+        const exists = await queryBuilder.select().where(conditions).getOne();
 
-      if (exists) {
-        throw new BadRequestException('Document already exists');
+        if (exists) {
+          throw new BadRequestException('Document already exists');
+        }
+
+        const entity = activeUser
+          ? {
+              ...createDto,
+              created_by: activeUser.sub,
+            }
+          : createDto;
+
+        const saved = await queryBuilder
+          .insert()
+          .into(this.genericRepository.metadata.name)
+          .values(entity)
+          .returning('*')
+          .execute();
+
+        return saved.generatedMaps[0] as T;
+      } catch (err) {
+        throw new ConflictException(err.message);
       }
-
-      const entity = activeUser
-        ? {
-            ...createDto,
-            created_by: activeUser.sub,
-          }
-        : createDto;
-
-      const saved = await queryBuilder
-        .insert()
-        .into(this.genericRepository.metadata.name)
-        .values(entity)
-        .returning('*')
-        .execute();
-
-      return saved.generatedMaps[0] as T;
     }
 
     public async findAll(
       conditions: Params = {},
       pagination: Pagination = { page: 1, limit: 10 },
     ): Promise<{ elements: T[]; pagination: PaginationResult }> {
-      const { page = 1, limit = 10 } = pagination;
-      const skipCount = (page - 1) * limit;
-      const order = conditions['order'] || 'created_at';
-      const select = conditions['select'] || [];
-      const relations = conditions['relations'] || [];
+      try {
+        const { page = 1, limit = 10 } = pagination;
+        const skipCount = (page - 1) * limit;
+        const order = conditions['order'] || 'created_at';
+        const select = conditions['select'] || [];
+        const relations = conditions['relations'] || [];
 
-      const where: Params = { ...conditions };
-      delete where.order;
-      delete where.relations;
-      delete where.select;
+        const where: Params = { ...conditions };
+        delete where.order;
+        delete where.relations;
+        delete where.select;
 
-      const queryBuilder = this.genericRepository.createQueryBuilder('entity');
-      if (where.deleted) {
-        queryBuilder.withDeleted();
-        delete where.deleted;
+        const queryBuilder =
+          this.genericRepository.createQueryBuilder('entity');
+        if (where.deleted) {
+          queryBuilder.withDeleted();
+          delete where.deleted;
+        }
+
+        queryBuilder
+          .select(select.length > 0 ? select : null)
+          .where(where)
+          .skip(skipCount)
+          .take(limit)
+          .orderBy(`entity.${order}`);
+
+        if (relations.length > 0) {
+          relations.forEach((relation) => {
+            queryBuilder.leftJoinAndSelect(`entity.${relation}`, relation);
+          });
+        }
+
+        const [elements, totalElements] = await queryBuilder.getManyAndCount();
+
+        const hasNextPage = totalElements > page * limit;
+        const nextPage = hasNextPage ? page + 1 : null;
+        const previousPage = page > 1 ? page - 1 : null;
+        const lastPage = Math.ceil(totalElements / limit);
+
+        const paginationResult: PaginationResult = {
+          totalElements,
+          hasNextPage,
+          nextPage,
+          previousPage,
+          lastPage,
+        };
+
+        return { elements, pagination: paginationResult };
+      } catch (err) {
+        throw new ConflictException(err.message);
       }
-
-      queryBuilder
-        .select(select.length > 0 ? select : null)
-        .where(where)
-        .skip(skipCount)
-        .take(limit)
-        .orderBy(`entity.${order}`);
-
-      if (relations.length > 0) {
-        relations.forEach((relation) => {
-          queryBuilder.leftJoinAndSelect(`entity.${relation}`, relation);
-        });
-      }
-
-      const [elements, totalElements] = await queryBuilder.getManyAndCount();
-
-      const hasNextPage = totalElements > page * limit;
-      const nextPage = hasNextPage ? page + 1 : null;
-      const previousPage = page > 1 ? page - 1 : null;
-      const lastPage = Math.ceil(totalElements / limit);
-
-      const paginationResult: PaginationResult = {
-        totalElements,
-        hasNextPage,
-        nextPage,
-        previousPage,
-        lastPage,
-      };
-
-      return { elements, pagination: paginationResult };
     }
 
     public async findAllWithoutPagination(
       conditions: Params = {},
     ): Promise<{ elements: T[]; total: number }> {
-      const order = conditions['order'] || 'created_at';
-      const select = conditions['select'] || [];
-      const relations = conditions['relations'] || [];
+      try {
+        const order = conditions['order'] || 'created_at';
+        const select = conditions['select'] || [];
+        const relations = conditions['relations'] || [];
 
-      const where: Params = { ...conditions };
-      delete where.order;
-      delete where.relations;
-      delete where.select;
+        const where: Params = { ...conditions };
+        delete where.order;
+        delete where.relations;
+        delete where.select;
 
-      const queryBuilder = this.genericRepository.createQueryBuilder('entity');
-      if (where.deleted) {
-        queryBuilder.withDeleted();
-        delete where.deleted;
+        const queryBuilder =
+          this.genericRepository.createQueryBuilder('entity');
+        if (where.deleted) {
+          queryBuilder.withDeleted();
+          delete where.deleted;
+        }
+
+        queryBuilder
+          .select(select.length > 0 ? select : null)
+          .where(where)
+          .orderBy(`entity.${order}`);
+
+        if (relations.length > 0) {
+          relations.forEach((relation) => {
+            queryBuilder.leftJoinAndSelect(`entity.${relation}`, relation);
+          });
+        }
+
+        const elements = await queryBuilder.getMany();
+        const total = await queryBuilder.getCount();
+
+        return { elements, total };
+      } catch (err) {
+        throw new ConflictException(err.message);
       }
-
-      queryBuilder
-        .select(select.length > 0 ? select : null)
-        .where(where)
-        .orderBy(`entity.${order}`);
-
-      if (relations.length > 0) {
-        relations.forEach((relation) => {
-          queryBuilder.leftJoinAndSelect(`entity.${relation}`, relation);
-        });
-      }
-
-      const elements = await queryBuilder.getMany();
-      const total = await queryBuilder.getCount();
-
-      return { elements, total };
     }
 
     public async findById(id: number): Promise<T> {
-      const queryBuilder = this.genericRepository.createQueryBuilder();
+      try {
+        const queryBuilder = this.genericRepository.createQueryBuilder();
 
-      const data = await queryBuilder.where({ id }).getOne();
+        const data = await queryBuilder.where({ id }).getOne();
 
-      if (!data)
-        throw new BadRequestException(
-          `Not found ${this.genericRepository.metadata.name} with id ${id}`,
-        );
+        if (!data)
+          throw new BadRequestException(
+            `Not found ${this.genericRepository.metadata.name} with id ${id}`,
+          );
 
-      return data;
+        return data;
+      } catch (err) {
+        throw new ConflictException(err.message);
+      }
     }
 
     public async findOne(conditions: Params): Promise<T> {
-      const where: Params = { ...conditions };
-      const relations = where.relations || [];
-      const select = conditions['select'] || '';
-      delete where.relations;
-      delete where.select;
+      try {
+        const where: Params = { ...conditions };
+        const relations = where.relations || [];
+        const select = conditions['select'] || '';
+        delete where.relations;
+        delete where.select;
 
-      const queryBuilder = this.genericRepository.createQueryBuilder('entity');
-      if (where.deleted) {
-        queryBuilder.withDeleted();
-        delete where.deleted;
+        const queryBuilder =
+          this.genericRepository.createQueryBuilder('entity');
+        if (where.deleted) {
+          queryBuilder.withDeleted();
+          delete where.deleted;
+        }
+
+        queryBuilder
+          .select(select.length > 0 ? select.map((s) => `entity.${s}`) : null)
+          .where(where);
+
+        if (relations.length > 0) {
+          relations.forEach((relation) => {
+            queryBuilder.leftJoinAndSelect(`entity.${relation}`, relation);
+          });
+        }
+
+        const response = await queryBuilder.getOne();
+
+        if (!response) {
+          throw new NotFoundException(
+            `Not found ${
+              this.genericRepository.metadata.name
+            } with conditions ${JSON.stringify(conditions)}`,
+          );
+        }
+
+        return response;
+      } catch (err) {
+        throw new ConflictException(err.message);
       }
-
-      queryBuilder
-        .select(select.length > 0 ? select.map((s) => `entity.${s}`) : null)
-        .where(where);
-
-      if (relations.length > 0) {
-        relations.forEach((relation) => {
-          queryBuilder.leftJoinAndSelect(`entity.${relation}`, relation);
-        });
-      }
-
-      const response = await queryBuilder.getOne();
-
-      if (!response) {
-        throw new NotFoundException(
-          `Not found ${
-            this.genericRepository.metadata.name
-          } with conditions ${JSON.stringify(conditions)}`,
-        );
-      }
-
-      return response;
     }
 
     public async exists(conditions: Params): Promise<boolean> {
-      const where: Params = { ...conditions };
+      try {
+        const where: Params = { ...conditions };
 
-      const queryBuilder = this.genericRepository.createQueryBuilder();
+        const queryBuilder = this.genericRepository.createQueryBuilder();
 
-      const count = await queryBuilder.where(where).getCount();
+        const count = await queryBuilder.where(where).getCount();
 
-      return count > 0;
+        return count > 0;
+      } catch (err) {
+        throw new ConflictException(err.message);
+      }
     }
 
     public async update(
       id: number,
       updateDto: Params,
       activeUser?: ActiveUserData,
-    ): Promise<T> {
-      const updateData = activeUser
-        ? { ...updateDto, updated_by: activeUser.sub }
-        : updateDto;
+      otp: UpdatedNew = { new: false },
+    ): Promise<T | any> {
+      try {
+        const updateData = activeUser
+          ? { ...updateDto, updated_by: activeUser.sub }
+          : updateDto;
 
-      const queryBuilder = this.genericRepository.createQueryBuilder();
+        const queryBuilder = this.genericRepository.createQueryBuilder();
 
-      await queryBuilder
-        .update()
-        .set(updateData as QueryDeepPartialEntity<T>)
-        .where('id = :id', { id })
-        .execute();
+        await queryBuilder
+          .update()
+          .set(updateData as QueryDeepPartialEntity<T>)
+          .where('id = :id', { id })
+          .execute();
 
-      const updatedEntity = await queryBuilder
-        .where('id = :id', { id })
-        .getOne();
-
-      if (!updatedEntity) {
-        throw new NotFoundException(
-          `Not found ${this.genericRepository.metadata.name} with id ${id}`,
-        );
+        if (otp.new) {
+          return await queryBuilder.where('id = :id', { id }).getOne();
+        }
+      } catch (err) {
+        throw new ConflictException(err.message);
       }
-
-      return updatedEntity;
     }
 
     public async updateMany(
       ids: number[],
       conditions: Params,
       activeUser?: ActiveUserData,
-    ): Promise<any> {
-      const updateData = activeUser
-        ? { ...conditions, updated_by: activeUser.sub }
-        : conditions;
+      otp: UpdatedNew = { new: false },
+    ): Promise<T[] | any> {
+      try {
+        const updateData = activeUser
+          ? { ...conditions, updated_by: activeUser.sub }
+          : conditions;
 
-      const queryBuilder = this.genericRepository.createQueryBuilder();
+        const queryBuilder = this.genericRepository.createQueryBuilder();
 
-      await queryBuilder
-        .update()
-        .set(updateData as QueryDeepPartialEntity<T>)
-        .whereInIds(ids)
-        .execute();
+        await queryBuilder
+          .update()
+          .set(updateData as QueryDeepPartialEntity<T>)
+          .whereInIds(ids)
+          .execute();
+
+        if (otp.new) {
+          return await queryBuilder.whereInIds(ids).getMany();
+        }
+      } catch (err) {
+        throw new ConflictException(err.message);
+      }
     }
 
     public async remove(id: number, activeUser?: ActiveUserData): Promise<any> {
-      const queryBuilder = this.genericRepository.createQueryBuilder();
+      try {
+        const queryBuilder = this.genericRepository.createQueryBuilder();
 
-      if (softDelete) {
-        await queryBuilder.softDelete().where('id = :id', { id }).execute();
-        await queryBuilder
-          .update()
-          .set({ restored_by: null, restored_at: null } as any)
-          .where('id = :id', { id })
-          .execute();
-
-        if (activeUser) {
+        if (softDelete) {
+          await queryBuilder.softDelete().where('id = :id', { id }).execute();
           await queryBuilder
             .update()
-            .set({ deleted_by: activeUser.sub } as any)
+            .set({ restored_by: null, restored_at: null } as any)
+            .where('id = :id', { id })
+            .execute();
+
+          if (activeUser) {
+            await queryBuilder
+              .update()
+              .set({ deleted_by: activeUser.sub } as any)
+              .where('id = :id', { id })
+              .execute();
+          }
+        } else {
+          await queryBuilder
+            .delete()
+            .from(this.genericRepository.metadata.name)
             .where('id = :id', { id })
             .execute();
         }
-      } else {
-        await queryBuilder
-          .delete()
-          .from(this.genericRepository.metadata.name)
-          .where('id = :id', { id })
-          .execute();
+      } catch (err) {
+        throw new ConflictException(err.message);
       }
     }
 
@@ -315,29 +353,33 @@ export function BaseService<T>(
       ids: number[],
       activeUser?: ActiveUserData,
     ): Promise<any> {
-      const queryBuilder = this.genericRepository.createQueryBuilder();
+      try {
+        const queryBuilder = this.genericRepository.createQueryBuilder();
 
-      if (softDelete) {
-        await queryBuilder.softDelete().whereInIds(ids).execute();
-        await queryBuilder
-          .update()
-          .set({ restored_by: null, restored_at: null } as any)
-          .whereInIds(ids)
-          .execute();
-
-        if (activeUser) {
+        if (softDelete) {
+          await queryBuilder.softDelete().whereInIds(ids).execute();
           await queryBuilder
             .update()
-            .set({ deleted_by: activeUser.sub } as any)
+            .set({ restored_by: null, restored_at: null } as any)
+            .whereInIds(ids)
+            .execute();
+
+          if (activeUser) {
+            await queryBuilder
+              .update()
+              .set({ deleted_by: activeUser.sub } as any)
+              .whereInIds(ids)
+              .execute();
+          }
+        } else {
+          await queryBuilder
+            .delete()
+            .from(this.genericRepository.metadata.name)
             .whereInIds(ids)
             .execute();
         }
-      } else {
-        await queryBuilder
-          .delete()
-          .from(this.genericRepository.metadata.name)
-          .whereInIds(ids)
-          .execute();
+      } catch (err) {
+        throw new ConflictException(err.message);
       }
     }
 
@@ -345,21 +387,25 @@ export function BaseService<T>(
       id: number,
       activeUser?: ActiveUserData,
     ): Promise<any> {
-      const queryBuilder = this.genericRepository.createQueryBuilder();
+      try {
+        const queryBuilder = this.genericRepository.createQueryBuilder();
 
-      await queryBuilder.restore().where('id = :id', { id }).execute();
-      await queryBuilder
-        .update()
-        .set({ restored_at: new Date(), deleted_by: null } as any)
-        .where('id = :id', { id })
-        .execute();
-
-      if (activeUser) {
+        await queryBuilder.restore().where('id = :id', { id }).execute();
         await queryBuilder
           .update()
-          .set({ restored_by: activeUser.sub } as any)
+          .set({ restored_at: new Date(), deleted_by: null } as any)
           .where('id = :id', { id })
           .execute();
+
+        if (activeUser) {
+          await queryBuilder
+            .update()
+            .set({ restored_by: activeUser.sub } as any)
+            .where('id = :id', { id })
+            .execute();
+        }
+      } catch (err) {
+        throw new ConflictException(err.message);
       }
     }
 
@@ -367,32 +413,40 @@ export function BaseService<T>(
       ids: number[],
       activeUser?: ActiveUserData,
     ): Promise<any> {
-      const queryBuilder = this.genericRepository.createQueryBuilder();
+      try {
+        const queryBuilder = this.genericRepository.createQueryBuilder();
 
-      await queryBuilder.restore().whereInIds(ids).execute();
-      await queryBuilder
-        .update()
-        .set({ restored_at: new Date(), deleted_by: null } as any)
-        .whereInIds(ids)
-        .execute();
-
-      if (activeUser) {
+        await queryBuilder.restore().whereInIds(ids).execute();
         await queryBuilder
           .update()
-          .set({ restored_by: activeUser.sub } as any)
+          .set({ restored_at: new Date(), deleted_by: null } as any)
           .whereInIds(ids)
           .execute();
+
+        if (activeUser) {
+          await queryBuilder
+            .update()
+            .set({ restored_by: activeUser.sub } as any)
+            .whereInIds(ids)
+            .execute();
+        }
+      } catch (err) {
+        throw new ConflictException(err.message);
       }
     }
 
     public async count(conditions: Params = {}): Promise<number> {
-      const where: Params = { ...conditions };
+      try {
+        const where: Params = { ...conditions };
 
-      const queryBuilder = this.genericRepository.createQueryBuilder();
+        const queryBuilder = this.genericRepository.createQueryBuilder();
 
-      const count = await queryBuilder.where(where).getCount();
+        const count = await queryBuilder.where(where).getCount();
 
-      return count;
+        return count;
+      } catch (err) {
+        throw new ConflictException(err.message);
+      }
     }
   }
 
